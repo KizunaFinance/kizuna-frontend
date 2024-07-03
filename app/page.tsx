498f7f91ae88171d0a3a28888e99a214f01b1d2e
Rohan Reddy <rohanreddy888@gmail.com>
2024-07-03 15:29:14 +0530

    Cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 import { Switch } from "@/components/ui/switch";
-import Image from "next/image";
 import { useState } from "react";
 
 export default function Home() {
