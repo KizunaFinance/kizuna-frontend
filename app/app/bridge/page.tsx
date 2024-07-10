d3a812cae2a0ccf609b8fb1681d44005df5eef7d
Rohan Reddy <rohanreddy888@gmail.com>
2024-07-10 19:20:41 +0530

    Added Chain Icon

--- app/app/bridge/page.tsx
+++ app/app/bridge/page.tsx
@@ -1,7 +1,26 @@
+"use client";
+import { Chains } from "@/app/providers/config";
+import Image from "next/image";
 export default function Home() {
   return (
-    <div className="flex flex-row justify-center items-center min-h-screen text-white">
+    <div className="flex flex-col gap-4 justify-center items-center min-h-screen text-white">
       <div>Bridge</div>
+      <div className="flex flex-row justify-center items-center gap-4">
+        {Chains.map((chain) => (
+          <div
+            className="flex flex-row justify-center items-center gap-2"
+            key={chain.id}
+          >
+            <Image
+              src={chain.iconUrl as string}
+              alt={chain.name}
+              width={20}
+              height={20}
+            />
+            <div>{chain.name}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- app/providers/config.ts
+++ app/providers/config.ts
@@ -1,41 +1,44 @@
-import { Chain, getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { mainnet, } from 'wagmi/chains';
+import { Chain, getDefaultConfig } from "@rainbow-me/rainbowkit";
 
 export const hekla: Chain = {
-    id: 167009,
-    name: 'Hekla',
-    nativeCurrency: {
-        name: 'Ethereum',
-        symbol: 'ETH',
-        decimals: 18
+  id: 167009,
+  name: "Hekla",
+  iconUrl: "/chains/icons/Taiko.svg",
+  nativeCurrency: {
+    name: "Ethereum",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.hekla.taiko.xyz"],
+      webSocket: undefined,
     },
-    rpcUrls: {
-        default: {
-            http: ['https://rpc.hekla.taiko.xyz'],
-            webSocket: undefined
-        }
-    }
-}
+  },
+};
 
 export const holesky: Chain = {
-    id: 17000,
-    name: 'Holesky',
-    nativeCurrency: {
-        name: 'Ethereum',
-        symbol: 'ETH',
-        decimals: 18
+  id: 17000,
+  name: "Holesky",
+  iconUrl: "/chains/icons/Ethereum.svg",
+  nativeCurrency: {
+    name: "Ethereum",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://1rpc.io/holesky"],
+      webSocket: undefined,
     },
-    rpcUrls: {
-        default: {
-            http: ['https://1rpc.io/holesky'],
-            webSocket: undefined
-        }
-    }
-}
+  },
+};
+
+export const Chains = [hekla, holesky];
 
 export const config = getDefaultConfig({
-    appName: 'My RainbowKit App',
-    projectId: 'YOUR_PROJECT_ID',
-    chains: [hekla, holesky],
-    ssr: true,
+  appName: "My RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
+  chains: [hekla, holesky],
+  ssr: true,
 });
