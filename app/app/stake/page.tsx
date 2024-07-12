d60488a72c39370ebf5494209254005059789d7a
Rohan Reddy <rohanreddy888@gmail.com>
2024-07-12 18:42:48 +0530

    Stake UI Improvements

--- app/app/stake/page.tsx
+++ app/app/stake/page.tsx
@@ -8,10 +8,18 @@ import { useAccount, useReadContract } from "wagmi";
 import { formatUnits } from "viem";
 
 import { switchChain } from "@wagmi/core";
-import { config } from "../../providers/config";
+import { Chains, config } from "../../providers/config";
 import { stakeToken, unstakeToken } from "../../utils/staking";
 import WalletConnect from "../../components/walletConnect";
 import { stakingABI } from "../../abi/stakingABI";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Image from "next/image";
 
 export default function Home() {
   const stakingToken = "0xdfa96d5E31177F182fc95790Be712D238d0d3b83";
@@ -21,6 +29,7 @@ export default function Home() {
 
   const { address, isConnected, chain } = useAccount();
   const inputAmount = useRef<HTMLInputElement>(null);
+  const [selectchain, setSelectchain] = useState<any>(Chains[0]);
   const {
     data: stakedBalances,
     isLoading: isStakedBalancesLoading,
@@ -72,33 +81,63 @@ export default function Home() {
     }
   };
 
+  function handleSelectChainChange(e: string) {
+    const chainID = parseInt(e);
+    const chain = Chains.find((c) => c.id === chainID);
+    if (chain) {
+      setSelectchain(chain);
+    }
+  }
+
   const [checked, setChecked] = useState(false);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-24  text-white">
-      <div className="flex flex-col items-center justify-center gap-8 max-w-xl w-full border-4 border-[#FF5D5D] rounded-3xl px-8 pb-10 pt-6">
-        <h1 className="text-3xl font-bold text-[#FF5D5D]">Stake ETH</h1>
-        <div className="flex flex-row justify-center items-center gap-2">
-          <Switch
-            onCheckedChange={async (e) => {
-              const chainResp = await _switchChain(e);
-              if (chainResp.success) {
-                setChecked(e);
-              } else {
-                alert("Please switch chain from your wallet");
-              }
-            }}
-            checked={checked}
-          />
-          <div>{checked ? "Ethereum" : "Taiko"}</div>
-        </div>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-12 p-24  text-white">
+      <div className="flex flex-col items-center justify-center gap-0 max-w-xl w-full border-4 border-[#FF5D5D] rounded-3xl px-8 pb-10 pt-6">
+        <h1 className="text-3xl font-bold text-[#FF5D5D] w-full text-left">
+          Stake ETH
+        </h1>
+
         <div className="flex flex-col justify-start items-start gap-4 w-full">
           <div className="flex flex-col justify-start items-start gap-2  w-full">
-            <Input
-              ref={inputAmount}
-              type="number"
-              className="w-full text-black py-5"
-              placeholder="Enter the amount"
-            />
+            <div className="flex flex-row justify-between items-center gap-2 w-full">
+              <div></div>
+              <div className="flex flex-row justify-center items-center gap-2 text-sm">
+                <div>Balance: 0.0011</div>
+                <button className="text-[#FF5D5D] font-bold">Max</button>
+              </div>
+            </div>
+            <div className="flex flex-row justify-start items-start gap-2  w-full">
+              <Select
+                value={selectchain.id.toString()}
+                onValueChange={(e) => handleSelectChainChange(e)}
+              >
+                <SelectTrigger className="w-36  bg-[#FF5D5D] py-2.5 px-2 rounded-lg border-0 font-medium focus:outline-none text-black">
+                  <SelectValue placeholder="Select" />
+                </SelectTrigger>
+                <SelectContent>
+                  {Chains.map((chain) => (
+                    <SelectItem key={chain.id} value={chain.id.toString()}>
+                      <div className="flex flex-row justify-center items-center gap-1">
+                        <Image
+                          className="bg-white rounded-full p-1"
+                          src={chain.iconUrl as string}
+                          alt={chain.name}
+                          width={25}
+                          height={25}
+                        />
+                        <div className="truncate w-fit">{chain.name}</div>
+                      </div>
+                    </SelectItem>
+                  ))}
+                </SelectContent>
+              </Select>
+              <Input
+                ref={inputAmount}
+                type="number"
+                className="py-2.5 rounded-lg bg-transparent focus:outline-none text-right w-full border border-slate-500 text-2xl"
+                placeholder={`0.1 - 0.5ETH`}
+              />
+            </div>
           </div>
           <Button
             onClick={async () => {
@@ -119,41 +158,59 @@ export default function Home() {
           </Button>
         </div>
 
-        <div className="grid grid-cols-2 justify-between items-start gap-4">
-          <div className="flex flex-col justify-center items-center gap-2">
-            <div>Staked on Taiko</div>
-            <div>
-              {formatUnits(stakedBalances ? stakedBalances : BigInt("0"), 18)}
-            </div>
-            {stakedBalances && stakedBalances > 0 && (
-              <div
-                className="text-red-500 cursor-pointer"
-                onClick={() => {
-                  unstakeToken(checked, stakedBalances, address!);
-                }}
-              >
-                Withdraw
+        <div className="flex flex-col items-start justify-center gap-4 w-full mt-6">
+          <h2 className="text-2xl font-bold text-[#FF5D5D]">Your Staking</h2>
+          <div className="grid grid-cols-2 justify-between items-start gap-4 w-full">
+            <div className="flex flex-col justify-center items-start gap-2">
+              <div>Staked on Ethereum</div>
+              <div>
+                {formatUnits(
+                  l1StakedBalances ? l1StakedBalances : BigInt("0"),
+                  18
+                )}
               </div>
-            )}
-          </div>
-          <div className="flex flex-col justify-center items-center gap-2">
-            <div>Staked on Ethereum</div>
-            <div>
-              {formatUnits(
-                l1StakedBalances ? l1StakedBalances : BigInt("0"),
-                18
+              {l1StakedBalances && l1StakedBalances > 0 && (
+                <div
+                  className="text-red-500 cursor-pointer"
+                  onClick={() => {
+                    unstakeToken(checked, l1StakedBalances, address!);
+                  }}
+                >
+                  Withdraw
+                </div>
               )}
             </div>
-            {l1StakedBalances && l1StakedBalances > 0 && (
-              <div
-                className="text-red-500 cursor-pointer"
-                onClick={() => {
-                  unstakeToken(checked, l1StakedBalances, address!);
-                }}
-              >
-                Withdraw
+            <div className="flex flex-col justify-center items-start gap-2">
+              <div>Staked on Taiko</div>
+              <div>
+                {formatUnits(stakedBalances ? stakedBalances : BigInt("0"), 18)}
               </div>
-            )}
+              {stakedBalances && stakedBalances > 0 && (
+                <div
+                  className="text-red-500 cursor-pointer"
+                  onClick={() => {
+                    unstakeToken(checked, stakedBalances, address!);
+                  }}
+                >
+                  Withdraw
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="max-w-xl w-full flex flex-col items-start justify-center gap-6">
+        <h1 className="text-3xl font-bold text-[#FF5D5D]">
+          Avaliable Liquidity
+        </h1>
+        <div className="grid grid-cols-2 gap-4 w-full">
+          <div className="flex flex-col justify-start items-start gap-2 border-4 border-[#FF5D5D] rounded-3xl px-8  py-6">
+            <h3 className=" text-xl font-medium">Ethereum</h3>
+            <h4 className="text-4xl font-bold text-[#FF5D5D]">1000 ETH</h4>
+          </div>
+          <div className="flex flex-col justify-start items-start gap-2 border-4 border-[#FF5D5D] rounded-3xl px-8  py-6">
+            <h3 className=" text-xl font-medium">Taiko</h3>
+            <h4 className="text-4xl font-bold text-[#FF5D5D]">1000 ETH</h4>
           </div>
         </div>
       </div>
