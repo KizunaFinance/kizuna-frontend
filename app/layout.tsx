55021896f681f09f381d9fc45dd143716d60f040
Rohan Reddy <rohanreddy888@gmail.com>
2024-07-15 03:52:51 +0530

    OG & SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,10 +8,42 @@ const outfit = Outfit({
   weight: ["200", "300", "400", "500", "600", "700", "800"],
 });
 
-export const metadata: Metadata = {
+export const metadata = {
   title: "Daiko Finance",
   description:
     "Daiko Finance is a decentralized bridge to send and receive crypto assets from and to Taiko & Ethereum.",
+  openGraph: {
+    title: "Daiko Finance",
+    url: "https://alpha.daiko.fi",
+    description:
+      "Daiko Finance is a decentralized bridge to send and receive crypto assets from and to Taiko & Ethereum.",
+    images: [
+      {
+        url: "https://alpha.daiko.fi/og/og-image.png",
+        secureUrl: "https://alpha.daiko.fi/og/og-image.png",
+        alt: "Daiko Finance",
+        width: 1200,
+        height: 630,
+        type: "image/png",
+      },
+    ],
+    locale: "en-US",
+    type: "website",
+  },
+  alternates: {
+    canonical: "https://alpha.daiko.fi",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Daiko Finance",
+    description:
+      "Daiko Finance is a decentralized bridge to send and receive crypto assets from and to Taiko & Ethereum.",
+    creator: "@DaikoFinance",
+    images: ["https://alpha.daiko.fi/og/og-image.png"],
+  },
+  robots: {
+    index: true,
+  },
 };
 
 export default function RootLayout({
